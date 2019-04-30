3c533b690dd5499c72cc5703310c1e4ae143a9f0
DOWRIGHT <dowright@nationofnobodies.com>
2019-04-29 20:16:22 -0700

    adjustments to dns relay, setting up for proxy integration...hopefully

--- dns_relay/dns_relay.py
+++ dns_relay/dns_relay.py
@@ -13,12 +13,11 @@ sys.path.insert(0, path)
 from socket import socket, AF_INET, SOCK_DGRAM
 from dnx_configure.dnx_system_info import Interface
 
-
 class DNSRelay:
     def __init__(self):
         self.path = os.environ['HOME_DIR']
         
-        with open('{}/data/config.json'.format(self.path), 'r') as settings:
+        with open(f'{self.path}/data/config.json', 'r') as settings:
             self.setting = json.load(settings)
 
         self.iniface = self.setting['Settings']['Interface']['Inside']
@@ -44,22 +43,20 @@ class DNSRelay:
         while True:
             for i, server in enumerate(self.dnsList, 1):
                 reach = subprocess.call(['sudo', 'ping', '-c', '1', server[0]], stdout = DEVNULL)
-                dnsserver = self.dnsserver['Server{}'.format(i)]
+                dnsserver = self.dnsserver[f'Server{i}']
                 if (reach == 0):
                     server[1] = True
                     status = 'UP'
-#                    print('{}:{}'.format(server[0], server[1]))
                 else:
                     server[1] = False
                     status = 'Down'
-#                    print('{}:{}'.format(server[0], server[1]))
+#               print(f'{server[0]}:{server[1]}')
+                self.dnsserver.update({f'Server{i}': {
+                                            'Name': dnsserver['Name'],
+                                            'IP Address': dnsserver['IP Address'],
+                                            'Status': status}})
 
-                self.dnsserver.update({'Server{}'.format(i): {
-                                            "Name": dnsserver['Name'],
-                                            "IP Address": dnsserver['IP Address'],
-                                            "Status": status}})
-
-            with open('{}/data/dnsstatus.json'.format(self.path), 'w') as dnsstat:
+            with open(f'{self.path}/data/dnsstatus.json', 'w') as dnsstat:
                 json.dump(self.dnsserver, dnsstat, indent=4)
         
             time.sleep(10)
@@ -68,74 +65,81 @@ class DNSRelay:
         try:        
             self.sock = socket(AF_INET, SOCK_DGRAM)
             self.sock.bind((self.laddr, self.lport))
-            # listen for UDP datagrams
-            print('[+] Listening -> {}:{}'.format(self.laddr, self.lport))
-            while True:
-                self.data, self.addr = self.sock.recvfrom(1024)
-#                start = time.time()
-                try:
-                    self.parse_init_query(self.data)
-                    
-                    if (self.qtype == b'\x01'):
-                        Relay = threading.Thread(target=self.RelayThread)
-                        Relay.daemon = True
-                        Relay.start()
-                    else:
-                        pass
-                except Exception as E:
-                    pass                    
         except Exception as E:
-            pass
+            print(f'Main Socket Error: {E}')
+
+        print(f'[+] Listening -> {self.laddr}:{self.lport}')
+        while True:
+            data_from_client, self.addr = self.sock.recvfrom(1024)
+            try:
+                packet = PacketManipulation(data_from_client)
+                packet.Parse()  
+                ## Matching IPV4 DNS queries only. All other will be dropped. Then creating a thread
+                ## to handle the rest of the process and sending client data in for relay to dns server        
+                if (packet.qtype == b'\x01'):
+                    Relay = threading.Thread(target=self.RelayThread, args=(data_from_client,))
+                    Relay.daemon = True
+                    Relay.start()
+            except Exception as E:
+                pass                    
             
-    def RelayThread(self):
+    def RelayThread(self, data_from_client):
         sock = socket(AF_INET, SOCK_DGRAM)
         ## -- 75 ms delay on all requests to give proxy more time to react -- ## Should be more tightly tuned
         time.sleep(.075)
         ## -------------- ##
+        ## Iterating over DNS Server List and Sending to first server that is available.
         for server in self.dnsList:
             if (server[1] == True):
-                sock.sendto(self.data, (server[0], 53))
-                print('Request Relayed to {}: {}'.format(server, 53))
-                data, _ = sock.recvfrom(1024)
-                data = self.parse_and_rewrite_query_response(data)
+                sock.sendto(data_from_client, (server[0], 53))
+                print(f'Request Relayed to {server}: 53')
+
+                ## Waiting for response from from then parsing packet and rewriting
+                ## TTL to 5 minutes.
+                data_from_server, _ = sock.recvfrom(1024)
+                packet = PacketManipulation(data_from_server)
+                packet.Parse()
+
+                ## Relaying packet from server back to host
+                packet_from_server = packet.send_data
                 print('Request Received')
                 break
-            else:
-                pass
-        self.sock.sendto(data, self.addr)
-               
-#        print('--------------------------')
-#        end = time.time()
-#        print(end - start)
-#        print('--------------------------')       
+        self.sock.sendto(packet_from_server, self.addr)     
 
-    def parse_init_query(self, data):
+class PacketManipulation:
+    def __init__(self, data):
+        self.data = data
+
+    def Parse(self):
+        self.ParseQuery()
+        self.ParseRewrite()
+
+    def ParseQuery(self):
 #        header = data[:12]
-        self.payload = data[12:]
+        self.payload = self.data[12:]
 #        tmp = struct.unpack(">6H", header)
         j = self.payload.index(0) + 1 + 4
         self.qtype = self.payload[j-3:j-2]
     
-    def parse_and_rewrite_query_response(self, data):
-        
-        qname = data[12:].split(b'\x00',1)[0]
-#        qname = qname[0]
+    def ParseRewrite(self):       
+        qname = self.data[12:].split(b'\x00',1)[0]
+
         b = 1
         for byte in qname[0:]:
             b += 1
             
         eoqname = 12+b
         eoquery = eoqname + 4        
-        rrecord = data[eoquery:]
+        rrecord = self.data[eoquery:]
         
         pointer = b'\xc0\x0c'
         ttl_bytes_override = b'\x00\x00\x01+'
         
         if (rrecord[0:2] == pointer):                
-            splitdata = data.split(pointer)
+            splitdata = self.data.split(pointer)
             rrname = pointer
         else:
-            splitdata = data.split(qname)
+            splitdata = self.data.split(qname)
             rrname = qname
             
         rr = b''
@@ -144,17 +148,15 @@ class DNSRelay:
                 pass
             else:
                 ttl_bytes = rrpart[4:8]
-                ttl_check = struct.unpack(">L", ttl_bytes)[0]
+                ttl_check = struct.unpack('>L', ttl_bytes)[0]
                 if (ttl_check > 299):
                     rr += rrname + rrpart[:4] + ttl_bytes_override + rrpart[8:]
                 else:
                     rr += rrname + rrpart
 
-        data = splitdata[0] + rr                    
-                
-        return data
+        self.send_data = splitdata[0] + rr
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:        
         DNS = DNSRelay()
         DNS.Start()
