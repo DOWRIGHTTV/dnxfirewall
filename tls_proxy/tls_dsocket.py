81f912941361a5ca8c8fe1e509d04c81781bf699
DOWRIGHT <dowright@nationofnobodies.com>
2019-05-09 11:38:20 -0700

    adjusted port

--- tls_proxy/tls_dsocket.py
+++ tls_proxy/tls_dsocket.py
@@ -5,7 +5,7 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 def TLSSocket():
     sock = socket(AF_INET, SOCK_STREAM)
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    sock.bind(('0.0.0.0', 6969))
+    sock.bind(('0.0.0.0', 4433))
 
     while True:
         sock.listen()
