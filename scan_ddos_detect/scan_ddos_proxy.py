1b89d76ac6cb4239ed5fc989edc1767b7946e20e
free <free@localhost.localdomain>
2019-06-05 22:39:49 -0700

    added tcp port scan detection | ddos detection foundation

--- scan_ddos_detect/scan_ddos_proxy.py
+++ scan_ddos_detect/scan_ddos_proxy.py
@@ -26,21 +26,37 @@ class SCANProxy:
         self.wan_int = self.setting['Settings']['Interface']['Outside']
         self.lan_int = 'eth0'
         self.wan_int = 'eth0'
+        Int = Interface()
+        self.wan_ip = Int.IP(self.wan_int)
 
         Int = Interface()
-        self.broadcast = Int.Broadcast(self.lan_int)
+        self.broadcast = Int.Broadcast(self.wan_int)
         # self.insideip = Int.IP(self.lan_int)
         self.DEVNULL = open(os.devnull, 'wb')
 
         self.udp_scan_tracker = {}
         self.tcp_scan_tracker = {}
-
         self.udp_scan_drop = {}
+        self.tcp_scan_drop = {}
+
+        self.open_udp_ports = {6969}
+        self.open_tcp_ports = {}
+
+        self.ddos_tracker = {}
+        self.active_ddos = {}
+
+        self.ddos_prevention = True
+        self.port_scan_logging = True
+        self.port_scan_prevention = True
                 
     def Start(self):       
         self.ProxyDB()
+
+        if (self.ddos_prevention):
+            threading.Thread(target=self.DDOS).start()
         
-        self.Proxy()
+        if (self.ddos_prevention or self.port_scan_logging):
+            self.Proxy()
 
     def ProxyDB(self):
         for table in {'DNSProxy', 'PIHosts'}:
@@ -51,65 +67,155 @@ class SCANProxy:
                     
     def Proxy(self):
     
-        Proxy = Sniffer(self.lan_int, action=self.SignatureCheck)
+        Proxy = Sniffer(self.wan_int, self.wan_ip, action=self.SignatureCheck)
         Proxy.Start()
        
     def SignatureCheck(self, packet):
-        try:
-            scan_detected = False
-            udp_scan_filter = False
+        src_ip = packet.src_ip
+        dst_ip = packet.dst_ip
+        dst_port = packet.dst_port
+        if (dst_ip == self.wan_ip and packet.protocol in {6} and dst_port in self.open_tcp_ports):
+            if (packet.tcp_syn and not packet.tcp_ack):
+                count = self.ddos_tracker[dst_ip].get(src_ip, 0)
+                count += 1
+                self.ddos_tracker[dst_ip].update({src_ip: count})
+        elif (dst_ip == self.wan_ip and packet.protocol in {17} and dst_port in self.open_udp_ports):
+                count = self.ddos_tracker[dst_ip].get(src_ip, 0)
+                count += 1
+                self.ddos_tracker[dst_ip].update({src_ip: count})
 
-            src_ip = packet.src_ip
-            dst_ip = packet.dst_ip
-            src_port = packet.src_port
-            dst_port = packet.dst_port
-            protocol = packet.protocol 
+        if (not self.active_ddos):
+            self.PortScan(packet)
 
-            if (protocol in {17} and dst_ip != self.broadcast):
-                udp_length = packet.udp_length
-                if (udp_length == 8):
+    def DDOS(self):
+        self.ddos_tracker[self.wan_ip] = {}
+        self.source_limit = 100 # Make this configurable
+        self.dest_limit = 200 # make this configurable
+        while True:
+            hosts = self.ddos_tracker[self.wan_ip]
+            for host, count in hosts.items():
+                if (count/5 > self.source_limit):
+                    self.active_ddos = True
+                    #BLOCK SOURCE IP WITH IPTABLE?
+                elif (count == 0):
+                    hosts.pop(host)
+                hosts[host] = 0
+            else:
+                self.active_ddos = False
+
+            time.sleep(5)
+
+    def PortScan(self, packet):
+        scan_detected = False
+        udp_scan_filter = False
+        tcp_scan_filter = False
+
+        src_ip = packet.src_ip
+        dst_ip = packet.dst_ip
+        src_port = packet.src_port
+        dst_port = packet.dst_port
+        protocol = packet.protocol
+
+        if (protocol in {17} and dst_ip != self.broadcast):
+            udp_length = packet.udp_length
+            print(f'{src_ip}:{src_port} | UDP | {dst_port}')
+            if (udp_length == 8):
+                scan_detected = True
+                udp_scan_filter = True
+            
+            if (src_ip not in self.udp_scan_tracker):
+                self.udp_scan_tracker[src_ip] = {dst_port}
+#                    threading.Thread(target=self.Timer, args=(src_ip,)).start()
+            else:
+                self.udp_scan_tracker[src_ip].update({dst_port})
+
+            if (src_ip in self.udp_scan_tracker):
+                connections = len(self.udp_scan_tracker.get(src_ip, 0))                 
+                if (connections >= 3):
                     scan_detected = True
                     udp_scan_filter = True
-                
-                if (src_ip not in self.udp_scan_tracker):
-                    self.udp_scan_tracker[src_ip] = {dst_port}
-                    threading.Thread(target=self.Timer, args=(src_ip,)).start()
+
+                if (udp_scan_filter and src_ip not in self.udp_scan_drop):
+                    self.udp_scan_drop[src_ip] = time.time()
+                    threading.Thread(target=self.UDPTimeout, args=(src_ip,)).start()
+                elif (udp_scan_filter and src_ip in self.udp_scan_drop):
+                    self.udp_scan_drop.update({src_ip: time.time()})
+
+        elif (protocol in {6} and dst_ip != self.broadcast):
+            if (src_ip != self.wan_ip and packet.tcp_syn and not packet.tcp_ack):
+                print(f'{src_ip}:{src_port} | SYN | {dst_port}')
+                if (src_ip not in self.tcp_scan_tracker):
+                    self.tcp_scan_tracker[src_ip] = {src_port: 1, dst_port: {'SYN': True, 'SYN/ACK': False, 'ACK': False}}
                 else:
-                    self.udp_scan_tracker[src_ip].update({dst_port})
+                    count = self.tcp_scan_tracker[src_ip].get(src_port)
+                    count += 1
+                    self.tcp_scan_tracker[src_ip].update({src_port: count})
+                    self.tcp_scan_tracker[src_ip].update({dst_port: {'SYN': True, 'SYN/ACK': False, 'ACK': False}})
+            elif (src_ip == self.wan_ip and packet.tcp_syn and packet.tcp_ack):
+                print(f'SYN/ACK: {dst_ip}')
+                self.tcp_scan_tracker[dst_ip].update({src_port: {'SYN/ACK': True}})
+                self.TCPTimer(dst_ip, src_port)
+            elif (src_ip != self.wan_ip and not packet.tcp_syn and packet.tcp_ack):
+                print(f'ACK: {dst_ip}')
+                self.tcp_scan_tracker[src_ip].update({dst_port: {'ACK': True}})
 
-                if (src_ip in self.udp_scan_tracker and not scan_detected):
-                    connections = len(self.udp_scan_tracker.get(src_ip, 0))
-                    if (connections >= 3):
-                        scan_detected = True
-                        udp_scan_filter = True
-                        if (src_ip not in self.udp_scan_drop):
-                            threading.Thread(target=self.Timeout, args=(src_ip,)).start()
-                        else:
-                            self.udp_scan_drop[src_ip].update(time.time())
+            if (src_ip in self.tcp_scan_tracker):
+                count = self.tcp_scan_tracker[src_ip].get(src_port, 0)
+                connections = len(self.tcp_scan_tracker.get(src_ip, 0))
+                if (count >= 2):
+                    scan_detected = True
+                    tcp_scan_filter = True
+                elif (connections >= 3):
+                    scan_detected = True
+                    tcp_scan_filter = True
+                elif (src_ip in self.tcp_scan_drop):
+                    scan_detected = True
+                    tcp_scan_filter = True
+                
+                if (tcp_scan_filter and src_ip not in self.tcp_scan_drop):
+                    self.tcp_scan_drop[src_ip] = time.time()
+                    threading.Thread(target=self.TCPTimeout, args=(src_ip,)).start()
+                elif (tcp_scan_filter and src_ip in self.tcp_scan_drop):
+                    self.tcp_scan_drop.update({src_ip: time.time()})
 
-            if (scan_detected):
-                print(f'SCAN DETECTED FROM: {src_ip}| PROTOCOL: {protocol} | PORT: {dst_port}')
+        if (scan_detected):
+            print(f'SCAN DETECTED FROM: {src_ip}| PROTOCOL: {protocol} | PORT: {dst_port}')
 
-            if (udp_scan_filter):
-                ICMP = ICMPResponse(self.wan_int, packet)
-                ICMP.Response()
+        if (self.port_scan_prevention or udp_scan_filter and dst_port in self.open_udp_ports):
+            ICMP = ICMPResponse(self.wan_int, packet)
+            ICMP.Response()
+        elif (self.port_scan_prevention or tcp_scan_filter and dst_port in self.open_tcp_ports):
+            #TCP = TCPResponse(self.wan_int, packet)
+            #TCP.Response()
+            pass
 
-        except Exception as E:
-            print(E)
+    def TCPTimer(self, dst_ip, src_port):
+        tcp_syn = self.tcp_scan_tracker[dst_ip][src_port].get('SYN')
+        tcp_syn_ack = self.tcp_scan_tracker[dst_ip][src_port].get('SYN/ACK')
+        time.sleep(1)
+        tcp_ack = self.tcp_scan_tracker[dst_ip][src_port].get('ACK')
+        if (tcp_syn and tcp_syn_ack and not tcp_ack):
+            self.tcp_scan_drop[dst_ip] = time.time()
 
-    def Timer(self, src_ip):
-        time.sleep(5)
-        self.udp_scan_tracker.pop(src_ip, None)
+    def TCPTimeout(self, src_ip):
+        while True:
+            now = time.time()
+            last_scan = self.tcp_scan_drop.get(src_ip, now)
+            if (now - last_scan >= 3):
+                self.tcp_scan_tracker.pop(src_ip, None)
+                self.tcp_scan_drop.pop(src_ip, None)
+                break
+            time.sleep(1)
 
-    def Timeout(self, src_ip)
-        self.udp_scan_drop[src_ip] = time.time()
+    def UDPTimeout(self, src_ip):
         while True:
             now = time.time()
             last_scan = self.udp_scan_drop.get(src_ip, now)
-            if (now - last_scan >= 10):
-                self.udp_scan_tracker.pop(src_ip, None))
+            if (now - last_scan >= 3):
+                self.udp_scan_tracker.pop(src_ip, None)
                 self.udp_scan_drop.pop(src_ip, None)
                 break
+            time.sleep(1)
 
 
         
--- scan_ddos_detect/scan_ddos_proxy_sniffer.py
+++ scan_ddos_detect/scan_ddos_proxy_sniffer.py
@@ -1,36 +1,61 @@
 #!/usr/bin/python3
 
+import os, sys
 import struct
 import binascii
 import codecs
+#ADD TRACEBAACK TO THIS SHIT
+
 
 from socket import socket, inet_aton, AF_PACKET, SOCK_RAW
+
+path = os.environ['HOME_DIR']
+sys.path.insert(0, path)
 
 from dnx_configure.dnx_exceptions import *
 
 class Sniffer:
-    def __init__(self, iface, action):
+    def __init__(self, wan_int, wan_ip, action):
         self.action = action
-        self.iface = iface
+        self.wan_int = wan_int
+        self.wan_ip = wan_ip
         self.s = socket(AF_PACKET, SOCK_RAW)
-        self.s.bind((self.iface, 3))
+        self.s.bind((self.wan_int, 3))
         
     def Start(self):
-        print(f'[+] Sniffing on: {self.iface}')
+        print(f'[+] Sniffing on: {self.wan_int}')
         while True:
+            send_to_proxy = False
             data, addr = self.s.recvfrom(1600)
             try:
                 Packet = PacketParse(data, addr)
                 Packet.Parse()
-                if (Packet.protocol in {6, 17}):
+                if (Packet.protocol in {6}):#, 17}):
+#                    if (Packet.dst_port <= 1024 or Packet.src_port <= 1024):
+                    if (Packet.dst_ip == self.wan_ip and Packet.tcp_syn and not Packet.tcp_ack):
+                        send_to_proxy = True
+                    elif (Packet.dst_ip == self.wan_ip and Packet.tcp_ack and not Packet.tcp_syn):
+                        send_to_proxy = True
+                    elif (Packet.src_ip == self.wan_ip and Packet.tcp_syn and Packet.tcp_ack):
+                        send_to_proxy = True
+                elif (Packet.protocol in {17}):
+                    if (Packet.dst_ip == self.wan_ip):
+                        send_to_proxy = True
+
+                if (send_to_proxy):
                     self.action(Packet)
             except DNXError:
                 pass
+            except Exception as E:
+                print(E)
                                         
 class PacketParse:
     def __init__(self, data, addr):
         self.data = data
         self.addr = addr
+
+        self.tcp_syn = False
+        self.tcp_ack = False
         
     def Parse(self):
         self.Ethernet()
@@ -43,7 +68,7 @@ class PacketParse:
         else:
             raise IPProtocolError('Packet protocol is not 6/TCP or 17/UDP')
 
-    def Ethernet(self):  
+    def Ethernet(self):
         s = []
         d = []
         smac = struct.unpack('!6c', self.data[6:12])
@@ -78,8 +103,23 @@ class PacketParse:
         self.udp_header = self.data[34:42]
 
     def TCP(self):
-        tcp_header = struct.unpack('!2H', self.data[34:38])
-        self.src_port = tcp_header[0]
-        self.dst_port = tcp_header[1]
-#        tcp_header = struct.unpack('!4H', self.data[34:])
+#        tcp_header = self.data[34:66]
+#        tcp_flags = tcp_header[14]
+        tcp_ports = struct.unpack('!2H', self.data[34:38]) #2LH
+        self.src_port = tcp_ports[0]
+        self.dst_port = tcp_ports[1]
+
+#        print(self.data[46:50])
+#        tcp_flags_all = {8: '', 7: '', 6: 'URG', 5: 'ACK', 4: 'PSH', 3: 'RST', 2: 'SYN', 1: 'FIN'}
+        if (self.data[47] & 1 << 1):
+            self.tcp_syn = True
+        if (self.data[47] & 1 << 4):
+            self.tcp_ack = True
+        # if (tcp_flags[0] & 1 << 1):
+        #     self.tcp_syn = True
+        # if (tcp_flags[0] & 1 << 4):
+        #     self.tcp_ack = True
+
+        
+                
         
