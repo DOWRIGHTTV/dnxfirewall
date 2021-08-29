ddbe4f6945181f9af8b508434732558def117059
DOWRIGHT <dowright@nationofnobodies.com>
2021-08-29 13:20:36 -0700

    cpacket testing

--- netfilter/setup.py
+++ netfilter/setup.py
@@ -1,54 +1,13 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
-import os
-
-VERSION = "0.8.1" # Remember to change CHANGES.txt and netfilterqueue.pyx when version changes.
-
-HOME_DIR = '/home/dnx/dnxfirewall'
-os.chdir(f'{HOME_DIR}/netfilter')
-
-try:
-    # Use Cython
-    from Cython.Distutils import build_ext
-    cmd = {"build_ext": build_ext}
-    ext = Extension(
-            "netfilterqueue",
-            sources=["netfilterqueue.pyx",],
-            libraries=["netfilter_queue"],
-        )
-except ImportError:
-    # No Cython
-    cmd = {}
-    ext = Extension(
-            "netfilterqueue",
-            sources = ["netfilterqueue.c"],
-            libraries=["netfilter_queue"],
-        )
+cmd = {'build_ext': build_ext}
+ext = Extension(
+    'dnx_nfqueue', sources=['dnx_nfqueue.pyx'], libraries=['netfilter_queue'])
 
 setup(
-    cmdclass = cmd,
-    ext_modules = [ext],
-    name="NetfilterQueue",
-    version=VERSION,
-    license="MIT",
-    author="Matthew Fox",
-    author_email="matt@tansen.ca",
-    url="https://github.com/kti/python-netfilterqueue",
-    description="Python bindings for libnetfilter_queue",
-    long_description=open("README.rst").read(),
-    download_url="http://pypi.python.org/packages/source/N/NetfilterQueue/NetfilterQueue-%s.tar.gz" % VERSION,
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: System :: Networking",
-        "Topic :: Security",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Telecommunications Industry",
-        "Programming Language :: Cython",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-    ]
+    name='DNX-NFQUEUE', cmdclass=cmd, ext_modules=cythonize(ext, language_level='3')
 )
