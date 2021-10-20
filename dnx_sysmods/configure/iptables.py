654e10d61b15cc786a8c33cd7c22a2dedda801b4
DOWRIGHT <dowright@nationofnobodies.com>
2021-10-20 12:17:19 -0700

    const to var.

--- dnx_sysmods/configure/iptables.py
+++ dnx_sysmods/configure/iptables.py
@@ -75,7 +75,7 @@ class _Defaults:
 
         # user configured services access will be kept as iptables for now. note: the implicit allows like dhcp and dns
         # will be handled by cfirewall from this point on. mark filter to ensure wan doesnt match as extra precaution.
-        shell(' iptables -m mark ! --mark 10 -j MGMT ')
+        shell(f'iptables -m mark ! --mark {WAN_IN} -j MGMT ')
 
         shell(f'iptables -A INPUT -p tcp  -j NFQUEUE --queue-num {Queue.CFIREWALL}')
         shell(f'iptables -A INPUT -p udp  -j NFQUEUE --queue-num {Queue.CFIREWALL}')
