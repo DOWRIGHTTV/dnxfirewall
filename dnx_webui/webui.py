36b26b95ed4ca1865ab00a16f74766e23e67bfb2
DOWRIGHT <dowright@nationofnobodies.com>
2022-02-05 15:52:31 -0700

    module navigation and import changes??

--- dnx_webui/webui.py
+++ dnx_webui/webui.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+
 from dnx_webui.source.main.dfe_main import app as application
 
 if (__name__ == '__main__'):
+    sys.path.insert(
+        0, os.environ.get('HOME_DIR', '/home/dnx/dnxfirewall')
+    )
+
     application.run()
