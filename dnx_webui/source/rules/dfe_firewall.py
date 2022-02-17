631d58b8e79e8eadaa7a8fbb6451456478a484aa
DOWRIGHT <dowright@nationofnobodies.com>
2022-02-17 02:43:50 -0700

    front end rework.

---
+++ dnx_webui/source/rules/dfe_firewall.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python3
+
+import json
+
+from collections import defaultdict, namedtuple
+from flask import Flask
+
+from dnx_gentools.def_constants import INVALID_FORM
+from dnx_gentools.file_operations import load_configuration
+from dnx_routines.configure.web_validate import ValidationError, convert_int
+
+from dnx_secmods.cfirewall.fw_manage import FirewallManage
+
+valid_sections = {
+    'BEFORE': '1',
+    'MAIN': '2',
+    'AFTER': '3',
+}
+
+reference_counts = defaultdict(int)
+zone_map = {'builtins': {}, 'extended': {}}
+zone_manager = {'builtins': {}, 'user-defined': {}}
+
+# including 0/any since it is not an actual zone definition
+_zone_map = {99: 'any'}
+
+def load_page(section='MAIN'):
+    global _zone_map
+
+    dnx_settings = load_configuration('system')
+
+    # building out interface to zone map NOTE: builtins only for now
+    for intf_type in ['builtins', 'extended']:
+        for intf_name, intf_info in dnx_settings.get_items(f'interfaces->{intf_type}'):
+            ident = intf_info['ident']
+
+            zone_map[intf_type][ident] = intf_name
+
+    # building zone list and reference counts NOTE: builtins only for now
+    for zone_type in ['builtins', 'user-defined']:
+        for zone_name, (zone_ident, zone_desc) in dnx_settings.get_items(f'zones->{zone_type}'):
+
+            # need to make converting zone ident/int to name easier in format function
+            _zone_map[zone_ident] = zone_name
+
+            zone_manager[zone_type][zone_name] = [reference_counts[zone_ident], zone_desc]
+
+    version, firewall_objects = Flask.app.dnx_object_manager.get_objects()
+
+    fw_object_map = {obj.name: {'type': obj.type, 'id': obj.id} for obj in firewall_objects}
+
+    network_autofill = {k.name: None for k in firewall_objects if k.type in ['country', 'address']}
+    service_autofill = {k.name: None for k in firewall_objects if k.type in ['service']}
+
+    firewall_rules = get_and_format_rules(section)
+
+    return {
+        'zone_map': zone_map,
+        'zone_manager': zone_manager,
+        'firewall_objects': firewall_objects,
+        'fw_object_map': fw_object_map,
+        'network_autofill': network_autofill,
+        'service_autofill': service_autofill,
+        'firewall_rules': firewall_rules,
+        'pending_changes': FirewallManage.is_pending_changes()
+    }
+
+def update_page(form):
+
+    # initial input validation for presence of zone field
+    section = form.get('section', None)
+    if (section not in valid_sections or 'change_section' not in form):
+        return INVALID_FORM, 'MAIN', load_page(section)
+
+    return None, section, load_page(section)
+
+def get_and_format_rules(section):
+    firewall_rules = FirewallManage.cfirewall.view_ruleset(section)
+
+    converted_rules = []
+    converted_rules_append = converted_rules.append
+
+    zone_map_get = _zone_map.get
+    obj_lookup = Flask.app.dnx_object_manager.lookup
+
+    # convert rules into a friendly format
+    for rule in firewall_rules.values():
+
+        src_zone = zone_map_get(rule['src_zone'][0], 'ERROR')
+        dst_zone = zone_map_get(rule['dst_zone'][0], 'ERROR')
+
+        # counting amount of times a zone is seen in a rule. default dict protects new zones ints.
+        reference_counts[src_zone] += 1
+        reference_counts[dst_zone] += 1
+
+        converted_rules_append([
+            rule['enabled'], rule['name'],
+
+            src_zone,
+            [obj_lookup(x) for x in rule['src_network']], [obj_lookup(x) for x in rule['src_service']],
+
+            dst_zone,
+            [obj_lookup(x) for x in rule['dst_network']], [obj_lookup(x) for x in rule['dst_service']],
+
+            rule['action'], rule['log'],
+            rule['ipp_profile'], rule['dns_profile'], rule['ips_profile']
+        ])
+
+    return converted_rules
+
+def commit_rules(json_data):
+
+    section = json_data.get('section', None)
+    if (not section or section not in valid_sections):
+        return False, {'error': True, 'message': 'missing section data'}
+
+    if not json_data.get('rules', None):
+        return False, {'error': True, 'message': 'missing rule data'}
+
+    # NOTE: all rules must be validated for any changes to be applied. validation will raise exception on first error.
+    try:
+        validated_rules = {section: validate_firewall_commit(json_data['rules'])}
+    except ValidationError as ve:
+        return False, {'error': True, 'message': str(ve)}
+
+    else:
+        FirewallManage.commit(validated_rules)
+
+    # temporary for testing
+    FirewallManage.push()
+
+    return True, {'error': False, 'message': None}
+
+
+# ================
+# VALIDATION
+# ================
+_rule_structure = namedtuple('rule_structure', [
+    'enabled', 'name',
+    'src_zone', 'src_network', 'src_service',
+    'dst_zone', 'dst_network', 'dst_service',
+    'action', 'log',
+    'sec1_prof', 'sec2_prof', 'sec3_prof'
+])
+
+def validate_firewall_commit(fw_rules, /):
+    # ["1", "lan_dhcp_allow", "lan", "tv,lan_network tv,dmz_network", "track_changes,udp_any", "any",
+    #  "tv,lan_network tv,dmz_network", "track_changes,udp_67", "ACCEPT", "OFF", "0", "0"]
+
+    fw_rules = json.loads(fw_rules)
+
+    validated_rules = {}
+
+    # index/ enumerate is for providing better feedback if issues are detected.
+    for i, rule in enumerate(fw_rules.values(), 1):
+
+        try:
+            rule = _rule_structure(*rule)
+        except ValueError:  # i think its a value error
+            raise ValidationError(f'Format error found in rule #{i}')
+
+        try:
+            validated_rules[i] = validate_firewall_rule(i, rule)
+        except ValidationError:
+            raise
+
+    print(validated_rules)
+    return validated_rules
+
+# NOTE: log disabled in form and set here as default for the time being.
+def validate_firewall_rule(rule_num, fw_rule, /):
+
+    # FASTER CHECKS FIRST
+    if (fw_rule.action not in ['ACCEPT', 'DROP']):
+        raise ValidationError(f'{INVALID_FORM} [rule #{rule_num}/action]')
+
+    action = 1 if fw_rule.action == 'ACCEPT' else 0
+
+    ip_proxy_profile = convert_int(fw_rule.sec1_prof)
+    ips_ids_profile  = convert_int(fw_rule.sec2_prof)
+    if not all([x in [0, 1] for x in [ip_proxy_profile, ips_ids_profile]]):
+        raise ValidationError(f'Invalid security profile for rule #{rule_num}.')
+
+    enabled = convert_int(fw_rule.enabled)
+
+    # OBJECT VALIDATIONS
+    check = Flask.app.dnx_object_manager.iter_validate
+
+    src_network = check(fw_rule.src_network)
+    if (None in src_network):
+        raise ValidationError(f'A source network object was not found for rule #{rule_num}.')
+
+    src_service = check(fw_rule.src_service)
+    if (None in src_service):
+        raise ValidationError(f'A source service object was not found for rule #{rule_num}.')
+
+    dst_network = check(fw_rule.dst_network)
+    if (None in dst_network):
+        raise ValidationError(f'A destination network object was not found for rule #{rule_num}.')
+
+    dst_service = check(fw_rule.src_service)
+    if (None in dst_service):
+        raise ValidationError(f'A destination service object was not found for rule #{rule_num}.')
+
+    # TODO: make zone map integrated better
+    dnx_interfaces = load_configuration('system').get_items('interfaces->builtins')
+    lzone_map = {zone_name: zone_info['zone'] for zone_name, zone_info in dnx_interfaces}
+
+    # 99 used to specify wildcard/any zone match
+    lzone_map['any'] = 99
+
+    s_zone = zone_map.get(fw_rule.src_zone, None)
+    d_zone = zone_map.get(fw_rule.dst_zone, None)
+    if (s_zone is None or d_zone is None):
+        raise ValidationError(f'{INVALID_FORM} [rule #{rule_num}/zone]')
+
+    rule = {
+        'name': fw_rule.name,
+        'id': None,
+        'enabled': enabled,
+        'src_zone': [s_zone],                # [12]
+        'src_network': src_network,
+        'src_service': src_service,
+        'dst_zone': [d_zone],                # [11]
+        'dst_network': dst_network,
+        'dst_service': dst_service,
+        'action': action,                    # 1
+        'log': 0,
+        'ipp_profile': ip_proxy_profile,
+        'ips_profile': ips_ids_profile
+    }
+
+    return rule
---
+++ dnx_webui/source/rules/dfe_xlist.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python3
+
+from dnx_gentools.def_constants import INVALID_FORM, fast_time
+from dnx_gentools.def_typing import *
+from dnx_gentools.def_enums import CFG, DATA
+from dnx_gentools.file_operations import ConfigurationManager, config, load_configuration
+
+from dnx_routines.configure.system_info import System
+from dnx_routines.configure.web_validate import ValidationError, VALID_DOMAIN, get_convert_int, standard, ip_address
+
+VALID_RULESETS = ['whitelist', 'blacklist']
+
+def load_page(form):
+    blacklist = load_configuration('blacklist')
+
+    for info in blacklist['time_based'].values():
+        st_offset = System.calculate_time_offset(info['time'])
+
+        info['time'] = System.format_date_time(st_offset)
+
+    blacklist_settings = {
+        'time_based': blacklist['time_based'],
+        'pre_proxy': blacklist['pre_proxy']
+    }
+
+    return blacklist_settings
+
+def update_page(form) -> Union[str, ValidationError]:
+    page_name = form.get('page_name', DATA.MISSING)
+    if (page_name is DATA.MISSING):
+        return INVALID_FORM
+
+    if ('xl_add' in form):
+        xlist_settings = config(**{
+            'domain': form.get('domain', DATA.MISSING),
+            'timer': get_convert_int(form, 'rule_length'),
+            'ruleset': page_name
+        })
+
+        if any([x in [DATA.MISSING, DATA.INVALID] for x in xlist_settings.values()]):
+            return INVALID_FORM
+
+        error = validate_time_based(xlist_settings)
+        if (error):
+            return error.message
+
+        configure_proxy_domain(xlist_settings, action=CFG.ADD)
+
+    elif ('xl_remove' in form):
+        xlist_settings = config(**{
+            'domain': form.get('bl_remove', DATA.MISSING),
+            'ruleset': page_name
+        })
+
+        if (DATA.MISSING in xlist_settings.values()):
+            return INVALID_FORM
+
+        configure_proxy_domain(xlist_settings, action=CFG.DEL)
+
+    elif ('exc_add' in form):
+        exception_settings = config(**{
+            'domain': form.get('domain', DATA.MISSING),
+            'reason': form.get('reason', DATA.MISSING),
+            'ruleset': page_name
+        })
+
+        if (DATA.MISSING in exception_settings.values()):
+            return INVALID_FORM
+
+        error = validate_pre_proxy_exc(exception_settings)
+        if (error):
+            return error.message
+
+        configure_pre_proxy_exc(exception_settings, action=CFG.ADD)
+
+    elif ('exc_remove' in form):
+        exception_settings = config(**{
+            'domain': form.get('exc_remove', DATA.MISSING),
+            'ruleset': page_name
+        })
+
+        # doing the iter for consistency
+        if (DATA.MISSING in exception_settings.values()):
+            return INVALID_FORM
+
+        configure_pre_proxy_exc(exception_settings, action=CFG.DEL)
+
+    else:
+        return INVALID_FORM
+
+    # =====================
+    # WHITELIST ONLY FORMS
+    # =====================
+
+    # TODO: this should not restrict ips to only "local_net" now that we have multiple interfaces.
+    # we should have the user select which interface it will be active on so we can properly validate the
+    # ip address falls within that subnet.
+
+    if ('ip_wl_add' in form):
+        whitelist_settings = config(**{
+            'ip': form.get('ip_wl_ip', DATA.MISSING),
+            'user':form.get('ip_wl_user', DATA.MISSING),
+            'type': form.get('ip_wl_type', DATA.MISSING)
+        })
+
+        if (DATA.MISSING in whitelist_settings.values()):
+            return INVALID_FORM
+
+        error = validate_ip_whitelist(whitelist_settings)
+        if (error):
+            return error.message
+
+        configure_ip_whitelist(whitelist_settings, action=CFG.ADD)
+
+    elif ('ip_wl_remove' in form):
+        whitelist_ip = form.get('ip_wl_ip', DATA.INVALID)
+
+        if (whitelist_ip is DATA.INVALID):
+            return INVALID_FORM
+
+        try:
+            ip_address(whitelist_ip)
+        except ValidationError as ve:
+            return ve
+
+        configure_ip_whitelist(whitelist_ip, action=CFG.DEL)
+
+    else:
+        return INVALID_FORM
+
+
+# ==============
+# VALIDATION
+# ==============
+
+def validate_time_based(settings: config) -> Optional[ValidationError]:
+    if (settings.ruleset not in VALID_RULESETS):
+        return ValidationError(INVALID_FORM)
+
+    if (not VALID_DOMAIN.match(settings.domain)):
+        return ValidationError('Domain name is not valid.')
+
+    if (not 1 <= settings.timer <= 1440):
+        return ValidationError('Timer must be between 1 and 1440 (24 hours).')
+
+def validate_pre_proxy_exc(settings: config) -> Optional[ValidationError]:
+    if (not VALID_DOMAIN.match(settings.domain)):
+        return ValidationError('Domain name is not valid.')
+
+    try:
+        standard(settings.reason)
+    except ValidationError as ve:
+        return ve
+
+def validate_ip_whitelist(settings: config) -> Optional[ValidationError]:
+    try:
+        ip_address(settings.ip)
+        standard(settings.user)
+    except ValidationError as ve:
+        return ve
+
+    if (settings.type != 'ip'):
+        return ValidationError(INVALID_FORM)
+
+# ==============
+# CONFIGURATION
+# ==============
+
+# adds a time based rule to whitelist/blacklist
+def configure_proxy_domain(settings: config, *, action: CFG):
+    input_time: int = int(fast_time())
+
+    with ConfigurationManager(settings.ruleset) as dnx:
+        domain_list = dnx.load_configuration()
+
+        if (action is CFG.ADD):
+
+            domain_list[f'time_based->{settings.domain}->time'] = input_time
+            domain_list[f'time_based->{settings.domain}->rule_length'] = settings.timer
+            domain_list[f'time_based->{settings.domain}->expire'] = input_time + settings.timer * 60
+
+        elif (action is CFG.DEL):
+            del domain_list[f'time_based->{settings.domain}']
+
+        dnx.write_configuration(domain_list.expanded_user_data)
+
+def configure_pre_proxy_exc(exc: config, *, action: CFG):
+    with ConfigurationManager(exc.ruleset) as dnx:
+        exceptions_list = dnx.load_configuration()
+
+        if (action is CFG.ADD):
+
+            exceptions_list[f'pre_proxy->{exc.domain}'] = exc.reason
+
+        elif (action is CFG.DEL):
+
+            del exceptions_list[exc.domain]
+
+        dnx.write_configuration(exceptions_list.expanded_user_data)
+
+def configure_ip_whitelist(ip_wl: config, /, *, action: CFG):
+    with ConfigurationManager('whitelist') as dnx:
+        whitelist = dnx.load_configuration()
+
+        if (action is CFG.ADD):
+            whitelist[f'ip_bypass->{ip_wl.ip}->user'] = ip_wl.user
+            whitelist[f'ip_bypass->{ip_wl.ip}->type'] = ip_wl.type
+
+        elif (action is CFG.DEL):
+            del whitelist[f'ip_bypass->{ip_wl.ip}']
+
+        dnx.write_configuration(whitelist.expanded_user_data)
