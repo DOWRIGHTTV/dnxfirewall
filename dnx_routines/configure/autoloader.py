0fc128f0e041625d44ad166622d19206b3a438c1
DOWRIGHT <dowright@nationofnobodies.com>
2022-03-10 15:55:34 -0700

    fixes after lab testing.

--- dnx_routines/configure/autoloader.py
+++ dnx_routines/configure/autoloader.py
@@ -12,31 +12,38 @@ import argparse
 from sys import argv
 from subprocess import run as srun, DEVNULL, CalledProcessError
 
-from dnx_gentools.def_constants import HOME_DIR, INIT_MODULE, hardout, str_join
+from dnx_gentools.def_constants import HOME_DIR, INITIALIZE_MODULE, hardout, str_join
 from dnx_gentools.file_operations import ConfigurationManager, load_data, write_configuration, json_to_yaml
+
 from dnx_routines.configure.iptables import IPTablesManager
 from dnx_routines.logging.log_client import Log
 
-LOG_NAME = 'system'
-PROGRESS_TOTAL_COUNT = 15
+# TYPING
+from dnx_gentools.file_operations import ConfigChain
 
-LINEBREAK = '-' * 32
 
-SYSTEM_DIR  = f'{HOME_DIR}/dnx_system'
-UTILITY_DIR = f'{HOME_DIR}/dnx_system/utils'
+LOG_NAME: str = 'system'
+PROGRESS_TOTAL_COUNT: int = 15
+
+LINEBREAK: str = '-' * 32
+
+SYSTEM_DIR:  str = f'{HOME_DIR}/dnx_system'
+UTILITY_DIR: str = f'{HOME_DIR}/dnx_system/utils'
 
 # ----------------------------
 # UTILS
 # ----------------------------
-def sprint(string):
-    '''setup print. includes timestamp before arg str.'''
-    print(f'{time.strftime("%H:%M:%S")}| {string}')
+def sprint(s: str, /) -> None:
+    '''setup print. includes timestamp before arg str.
+    '''
+    print(f'{time.strftime("%H:%M:%S")}| {s}')
 
-def eprint(string):
-    '''error print. includes timestamp and alert before arg str.'''
-    print(f'{time.strftime("%H:%M:%S")}| !!! {string}')
+def eprint(s: str, /) -> None:
+    '''error print. includes timestamp and alert before arg str.
+    '''
+    print(f'{time.strftime("%H:%M:%S")}| !!! {s}')
     while True:
-        answer = input('continue? [y/N]: ')
+        answer: str = input('continue? [y/N]: ')
         if (answer.lower() == 'y'):
             return
 
@@ -46,37 +53,38 @@ def eprint(string):
         else:
             print('!invalid selection.')
 
-def dnx_run(string):
-    '''convenience function, subprocess run wrapper adding additional args.'''
+def dnx_run(s: str, /) -> None:
+    '''convenience function, subprocess run wrapper adding additional args.
+    '''
     try:
         if (VERBOSE):
-            srun(string, shell=True, check=True)
+            srun(s, shell=True, check=True)
 
         else:
-            srun(string, shell=True, stdout=DEVNULL, stderr=DEVNULL, check=True)
+            srun(s, shell=True, stdout=DEVNULL, stderr=DEVNULL, check=True)
 
     except CalledProcessError as cpe:
-        eprint(cpe)
+        eprint(f'{cpe}')
 
-def check_run_as_root():
+def check_run_as_root() -> None:
     if (os.getuid()):
 
         eprint('must run dnxfirewall auto loader as root. exiting...')
 
-def check_dnx_user():
-    with open('/etc/passwd') as passwd:
-        passwd = passwd.read().splitlines()
+def check_dnx_user() -> None:
+    with open('/etc/passwd', 'r') as passwd_f:
+        passwd: list[str] = passwd_f.read().splitlines()
 
     if not any([usr for usr in passwd if usr.split(':', 1)[0] == 'dnx']):
 
         eprint('dnx user does not exist. create user and clone repo into dnx home directory before running.')
 
-def check_clone_location():
+def check_clone_location() -> None:
     if (not os.path.isdir(HOME_DIR)):
 
         eprint('dnxfirewall filesystem must be located at /home/dnx.')
 
-def check_already_ran():
+def check_already_ran() -> None:
     with ConfigurationManager('system') as dnx:
         dnx_settings = dnx.load_configuration()
 
@@ -88,57 +96,57 @@ def check_already_ran():
 # ----------------------------
 # PROGRESS BAR
 # ----------------------------
-# starting at -1 to compensate for first process
-completed_count, p_total = -1, PROGRESS_TOTAL_COUNT
-def progress(desc):
+# starting at -1 to compensate for the first process
+bar_len: int = 30
+completed_count: int = -1
+def progress(desc: str) -> None:
     global completed_count
 
     completed_count += 1
+    ratio: float = completed_count / PROGRESS_TOTAL_COUNT
 
-    bar_len = 30
+    filled_len: int = int(bar_len * ratio)
+    bar: str = '#' * filled_len + '=' * (bar_len - filled_len)
 
-    filled_len = int(round(bar_len * completed_count / float(p_total)))
-    percents = round(100.0 * completed_count / float(p_total), 1)
-
-    bar = str_join(['#' * filled_len, '=' * (bar_len - filled_len)])
-    sys.stdout.write(f'{" "*90}\r')
-    sys.stdout.write(f'{completed_count}/{p_total} || [{bar}] {percents}% || {desc}\r')
+    sys.stdout.write(f'{completed_count}/{PROGRESS_TOTAL_COUNT} |')
+    sys.stdout.write(f'| [{bar}] {int(100 * ratio)}% |')
+    sys.stdout.write(f'| {desc}{" "*20}\r')
     sys.stdout.flush()
 
 # ============================
 # INTERFACE CONFIGURATION
 # ============================
 # convenience function wrapper for physical interface to dnxfirewall zone association.
-def configure_interfaces():
-    interfaces_detected = check_system_interfaces()
+def configure_interfaces() -> None:
+    interfaces_detected: list[str] = check_system_interfaces()
 
-    user_intf_config = collect_interface_associations(interfaces_detected)
-    public_dns_servers = load_data('dns_server')['resolvers']
+    user_intf_config: dict[str, str] = collect_interface_associations(interfaces_detected)
+    public_dns_servers: dict = load_data('dns_server')['resolvers']
 
     set_dnx_interfaces(user_intf_config)
     set_dhcp_interfaces(user_intf_config)
 
-    with open(f'{SYSTEM_DIR}/interfaces/intf_config_template.cfg', 'r') as intf_configs:
-        intf_configs = intf_configs.read()
+    with open(f'{SYSTEM_DIR}/interfaces/intf_config_template.cfg', 'r') as intf_configs_f:
+        intf_configs: str = intf_configs_f.read()
 
     for intf_name, intf in user_intf_config.items():
         intf_configs = intf_configs.replace(f'_{intf_name}_', intf)
 
-    # storing modified template containing specified interface names. this will be used to configure
-    # wan interface via webui or change system level dns servers.
+    # storing the modified template containing specified interface names.
+    # this will be used to configure wan interface via webui or change system level dns servers.
     write_configuration(json.loads(intf_configs), 'intf_config', filepath='dnx_system/interfaces')
 
     # setting public dns servers on the interface so the system itself will use the user configured
     # servers in the web ui.
-    dns1 = public_dns_servers['primary']['ip_address']
-    dns2 = public_dns_servers['secondary']['ip_address']
+    dns1: str = public_dns_servers['primary']['ip_address']
+    dns2: str = public_dns_servers['secondary']['ip_address']
 
-    yaml_output = json_to_yaml(intf_configs, is_string=True)
+    yaml_output: str = json_to_yaml(intf_configs, is_string=True)
     yaml_output = yaml_output.replace('_PRIMARY__SECONDARY_', f'{dns1},{dns2}')
 
     write_net_config(yaml_output)
 
-def check_system_interfaces():
+def check_system_interfaces() -> list[str]:
     interfaces_detected = [intf[1] for intf in socket.if_nameindex() if 'lo' not in intf[1]]
 
     if (len(interfaces_detected) < 3):
@@ -146,10 +154,8 @@ def check_system_interfaces():
 
     return interfaces_detected
 
-def collect_interface_associations(interfaces_detected):
-    print(LINEBREAK)
-    print('available interfaces')
-    print(LINEBREAK)
+def collect_interface_associations(interfaces_detected: list[str]) -> dict[str, str]:
+    print(f'{LINEBREAK}\navailable interfaces\n{LINEBREAK}')
 
     for i, interface in enumerate(interfaces_detected, 1):
         print(f'{i}. {interface}')
@@ -157,7 +163,7 @@ def collect_interface_associations(interfaces_detected):
     print(LINEBREAK)
 
     # build out full json for interface configs as dict
-    interface_config = {'WAN': None, 'LAN': None, 'DMZ': None}
+    interface_config: dict[str, str] = {'WAN': '', 'LAN': '', 'DMZ': ''}
     while True:
         for int_name in interface_config:
             while True:
@@ -175,34 +181,34 @@ def collect_interface_associations(interfaces_detected):
     return interface_config
 
 # takes interface config as dict, converts to yaml, then writes to system folder
-def write_net_config(interface_configs):
+def write_net_config(interface_configs: str) -> None:
     sprint('configuring netplan service...')
 
     # write config file to netplan
     with open('/etc/netplan/01-dnx-interfaces.yaml', 'w') as intf_config:
         intf_config.write(interface_configs)
 
-    # removing configuration set during os install.
+    # removing the default configuration set during os install.
     try:
         os.remove('/etc/netplan/00-installer-config.yaml')
     except:
         pass
 
-# modifying dnx configuration files with user specified interface names and their corresponding zones
-def set_dnx_interfaces(user_intf_config):
+# modifying dnx configuration files with the user specified interface names and their corresponding zones
+def set_dnx_interfaces(user_intf_config: dict[str, str]) -> None:
     sprint('setting dnx interface configurations...')
 
     with ConfigurationManager('system') as dnx:
-        dnx_settings = dnx.load_configuration()
+        dnx_settings: ConfigChain = dnx.load_configuration()
 
         for zone, intf in user_intf_config.items():
             dnx_settings[f'interfaces->builtins->{zone.lower()}->ident'] = intf
 
         dnx.write_configuration(dnx_settings.expanded_user_data)
 
-def set_dhcp_interfaces(user_intf_config):
+def set_dhcp_interfaces(user_intf_config: dict[str, str]) -> None:
     with ConfigurationManager('dhcp_server') as dhcp:
-        dhcp_settings = dhcp.load_configuration()
+        dhcp_settings: ConfigChain = dhcp.load_configuration()
 
         for zone in ['LAN', 'DMZ']:
 
@@ -210,7 +216,7 @@ def set_dhcp_interfaces(user_intf_config):
 
         dhcp.write_configuration(dhcp_settings.expanded_user_data)
 
-def confirm_interfaces(interface_config):
+def confirm_interfaces(interface_config: dict[str, str]) -> bool:
     print(' '.join([f'{zone}={intf}' for zone, intf in interface_config.items()]))
     while True:
         answer = input('confirm? [Y/n]: ')
@@ -223,7 +229,7 @@ def confirm_interfaces(interface_config):
 # ============================
 # INSTALL PACKAGES
 # ============================
-def install_packages():
+def install_packages() -> None:
 
     commands = [
         ('sudo apt install python3-pip -y', 'setting up python3'),
@@ -240,7 +246,7 @@ def install_packages():
 
         dnx_run(command)
 
-def compile_extensions():
+def compile_extensions() -> None:
 
     commands = [
         (f'sudo python3 {UTILITY_DIR}/compile_trie_search.py build_ext --inplace', 'compiling trie search C extension'),
@@ -254,7 +260,7 @@ def compile_extensions():
 
         dnx_run(command)
 
-def configure_webui():
+def configure_webui() -> None:
     cert_subject = str_join([
         '/C=US',
         '/ST=Arizona',
@@ -291,7 +297,7 @@ def configure_webui():
 # PERMISSION CONFIGURATION
 # ============================
 
-def set_permissions():
+def set_permissions() -> None:
 
     progress('configuring dnxfirewall permissions')
 
@@ -326,7 +332,7 @@ def set_permissions():
 # ============================
 # SERVICE FILE SETUP
 # ============================
-def set_services():
+def set_services() -> None:
     ignore_list = ['dnx-database-psql.service', 'dnx-syslog.service']
 
     progress('creating dnxfirewall services')
@@ -345,7 +351,7 @@ def set_services():
 # ============================
 # INITIAL IPTABLES SETUP
 # ============================
-def configure_iptables():
+def configure_iptables() -> None:
     progress('loading default iptables')
 
     with IPTablesManager() as iptables:
@@ -354,9 +360,9 @@ def configure_iptables():
 # ============================
 # CLEANUP
 # ============================
-def mark_completion_flag():
+def mark_completion_flag() -> None:
     with ConfigurationManager('system') as dnx:
-        dnx_settings = dnx.load_configuration()
+        dnx_settings: ConfigChain = dnx.load_configuration()
 
         dnx_settings['auto_loader'] = True
 
