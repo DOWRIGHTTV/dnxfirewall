6947ef463afce45311bf3f5f51ea4a9f6e59db05
DOWRIGHT <dowright@nationofnobodies.com>
2022-03-17 20:44:04 -0700

    webui fixes

--- dnx_system/utils/web_run.sh
+++ dnx_system/utils/web_run.sh
@@ -2,5 +2,6 @@
 
 export FLASK_APP=/home/free/Desktop/new_repos/dnxfirewall-cmd/dnx_webui/source/main/dfe_main.py
 export FLASK_ENV=development
+export PYTHONPATH=/home/free/Desktop/new_repos/dnxfirewall-cmd/dnx_webui:$PYTHONPATH
 export HOME_DIR=/home/free/Desktop/new_repos/dnxfirewall-cmd
-flask run --host=192.168.5.179 --port=$1
+flask run --host=192.168.5.179 --port="$1"
