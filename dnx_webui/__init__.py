38241c8582de8e08b06fdb6e99b3f705a624b364
DOWRIGHT <dowright@nationofnobodies.com>
2022-03-19 01:01:40 -0700

    webui fixes after lab testing

--- dnx_webui/__init__.py
+++ dnx_webui/__init__.py
@@ -10,13 +10,6 @@ from dnx_gentools.def_constants import INITIALIZE_MODULE
 if INITIALIZE_MODULE('webui'):
     __all__ = ('app',)
 
-    import os
-    import sys
-
-    HOME_DIR = os.environ.get('HOME_DIR', '/home/dnx/dnxfirewall')
-
-    sys.path.insert(0, f'{HOME_DIR}/dnx_webui')
-
     # app will be called by uwsgi
     from source.main.dfe_main import app
 
