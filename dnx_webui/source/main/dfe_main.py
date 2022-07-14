f2e5ef8870dc99c424e664ed036f7959c80c0067
DOWRIGHT <dow@dnxfirewall.com>
2022-07-13 20:22:45 -0700

    webui traffic logging tweaks to accommodate timestamp.

--- dnx_webui/source/main/dfe_main.py
+++ dnx_webui/source/main/dfe_main.py
@@ -404,7 +404,7 @@ def system_logs_traffic(session_data):
     page_settings = {
         'navi': True, 'idle_timeout': True, 'log_timeout': True, 'standard_error': None,
         'menu': '1', 'table': '1', 'dnx_table': True, 'ajax': False, 'auto_colorize': True,
-        'table_types': ['firewall', 'nat'],
+        'table_types': ['firewall', '.nat'],
         'uri_path': ['system', 'log', 'traffic']
     }
 
