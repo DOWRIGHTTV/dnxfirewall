4bdd5acaedb15d1d4e5f20e6a8d649404fe1fc10
DOWRIGHT <dow@dnxfirewall.com>
2022-07-13 20:12:43 -0700

    webui traffic logging tweaks to accomodate timestamp.

--- dnx_webui/source/system/log/dfe_traffic.py
+++ dnx_webui/source/system/log/dfe_traffic.py
@@ -16,6 +16,7 @@ LOG_FILES = [
     'firewall', 'nat',
 ]
 
+
 class FIREWALL_LOG(_NamedTuple):
     timestamp: str
     type: str
@@ -62,7 +63,6 @@ def get_log_entries(file_path: str) -> list[FIREWALL_LOG]:
     combined_formatted = []
     combinedf_append = combined_formatted.append
     for entry in combined_raw[:100]:
-        print([sub_entry for sub_entry in entry.split()])
-        combinedf_append(FIREWALL_LOG(*[sub_entry.replace('"', '').split('=') for sub_entry in entry.split()][1]))
+        combinedf_append(FIREWALL_LOG(*[sub_entry.replace('"', '').split('=')[1] for sub_entry in entry.split()][1]))
 
     return combined_formatted
