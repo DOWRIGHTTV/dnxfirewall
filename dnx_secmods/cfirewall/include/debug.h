a4c9f408fb8cd35b6c64bdb15879e859424daabf
DOWRIGHT <dow@dnxfirewall.com>
2022-07-22 10:26:08 -0700

    C debugging pre compile tweaks.

--- dnx_secmods/cfirewall/include/debug.h
+++ dnx_secmods/cfirewall/include/debug.h
@@ -2,7 +2,19 @@
 #define DEBUG_H
 
 #define DEVELOPMENT 1
+
 // trailing underscore to reduce chance of conflicting name defined elsewhere.
-#define debug_(on, fmt, ...) do { if (DEVELOPMENT && on) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0);
+#define debug_(...) do { if (DEVELOPMENT) debug__(...); } while (0)
+
+extern void debug__(int on, char* fmt, ...);
+
+void
+debug__(int on, char* fmt, ...)
+{
+    if (on) {
+        fprintf(stderr, fmt, ##__VA_ARGS__);
+    }
+}
+
 
 #endif
