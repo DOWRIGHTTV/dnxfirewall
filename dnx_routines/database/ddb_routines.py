2f2cdc6490599bf09cbfbb24bb063e378e5cd865
DOWRIGHT <dowright@nationofnobodies.com>
2022-09-13 12:32:50 -0700

    typing and minor refactor

--- dnx_routines/database/ddb_routines.py
+++ dnx_routines/database/ddb_routines.py
@@ -35,6 +35,8 @@ if (TYPE_CHECKING):
     from dnx_gentools.def_namedtuples import IPP_EVENT_LOG, DNS_REQUEST_LOG, IPS_EVENT_LOG, GEOLOCATION_LOG
     from dnx_gentools.def_namedtuples import INF_EVENT_LOG
 
+    from sqlite3 import Cursor
+
 
 db = _db_conn.DBConnector
 
@@ -43,7 +45,7 @@ db = _db_conn.DBConnector
 # ========================================
 @db.register('dns_request', routine_type='write')
 # standard input for dns proxy module database entries
-def dns_request(cur, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
+def dns_request(cur: Cursor, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
     cur.execute(
         f'select * from dnsproxy where src_ip=? and domain=? and action=?', (log.src_ip, log.request, log.action)
     )
@@ -53,11 +55,14 @@ def dns_request(cur, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
 
         i, t = existing_record[5] + 1, existing_record[6]
         if (timestamp - t > 10):
-            cur.execute(f'update dnsproxy set count=?, last_seen=?, reason=? where src_ip=? and domain=? and action=?',
-                (i, timestamp, log.reason, log.src_ip, log.request, log.action))
+            cur.execute(
+                f'update dnsproxy set count=?, last_seen=?, reason=? where src_ip=? and domain=? and action=?',
+                (i, timestamp, log.reason, log.src_ip, log.request, log.action)
+            )
 
     else:
-        cur.execute(f'insert into dnsproxy values (?, ?, ?, ?, ?, ?, ?)',
+        cur.execute(
+            f'insert into dnsproxy values (?, ?, ?, ?, ?, ?, ?)',
             (log.src_ip, log.request, log.category, log.reason, log.action, 1, timestamp)
         )
 
@@ -65,7 +70,7 @@ def dns_request(cur, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
 
 @db.register('dns_blocked', routine_type='write')
 # used by dns proxy to authorize front end block page access.
-def dns_blocked(cur, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
+def dns_blocked(cur: Cursor, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
     cur.execute(
         f'insert into blocked values (?, ?, ?, ?, ?)', (log.src_ip, log.request, log.category, log.reason, timestamp)
     )
@@ -74,9 +79,9 @@ def dns_blocked(cur, timestamp: int, log: DNS_REQUEST_LOG) -> bool:
 
 @db.register('ips_event', routine_type='write')
 # standard input for ips module database entries
-def ips_event(cur, timestamp: int, log: IPS_EVENT_LOG) -> bool:
-    cur.execute(f'select * from ips where src_ip=? and attack_type=? order by last_seen desc limit 1',
-        (log.attacker, log.attack_type)
+def ips_event(cur: Cursor, timestamp: int, log: IPS_EVENT_LOG) -> bool:
+    cur.execute(
+        f'select * from ips where src_ip=? and attack_type=? order by last_seen desc limit 1', (log.attacker, log.attack_type)
     )
 
     existing_record = cur.fetchone()
@@ -84,12 +89,14 @@ def ips_event(cur, timestamp: int, log: IPS_EVENT_LOG) -> bool:
 
         t = existing_record[4]
         if (timestamp - t > 10):
-            cur.execute(f'insert into ips values (?, ?, ?, ?, ?)',
+            cur.execute(
+                f'insert into ips values (?, ?, ?, ?, ?)',
                 (log.attacker, log.protocol, log.attack_type, log.action, timestamp)
             )
 
     else:
-        cur.execute(f'insert into ips values (?, ?, ?, ?, ?)',
+        cur.execute(
+            f'insert into ips values (?, ?, ?, ?, ?)',
             (log.attacker, log.protocol, log.attack_type, log.action, timestamp)
         )
 
@@ -97,27 +104,29 @@ def ips_event(cur, timestamp: int, log: IPS_EVENT_LOG) -> bool:
 
 @db.register('ipp_event', routine_type='write')
 # standard input for ip proxy module database entries.
-def ipp_event(cur, timestamp: int, log: IPP_EVENT_LOG) -> bool:
-    cur.execute(f'insert into ipproxy values (?, ?, ?, ?, ?, ?)',
-        (log.local_ip, log.tracked_ip, '/'.join(log.category), log.direction, log.action, timestamp))
+def ipp_event(cur: Cursor, timestamp: int, log: IPP_EVENT_LOG) -> bool:
+    cur.execute(
+        f'insert into ipproxy values (?, ?, ?, ?, ?, ?)',
+        (log.local_ip, log.tracked_ip, '/'.join(log.category), log.direction, log.action, timestamp)
+    )
 
     return True
 
 @db.register('inf_event', routine_type='write')
-def infected_event(cur, timestamp: int, log: INF_EVENT_LOG) -> bool:
-    cur.execute(f'select * from infectedclients where mac=? and detected_host=?',
-        (log.client_mac, log.detected_host)
-    )
+def infected_event(cur: Cursor, timestamp: int, log: INF_EVENT_LOG) -> bool:
+    cur.execute(f'select * from infectedclients where mac=? and detected_host=?', (log.client_mac, log.detected_host))
 
     existing_record = cur.fetchone()
     if (existing_record):
 
-        cur.execute(f'update infectedclients set last_seen=? where mac=? and detected_host=?',
+        cur.execute(
+            f'update infectedclients set last_seen=? where mac=? and detected_host=?',
             (timestamp, log.client_mac, log.detected_host)
         )
 
     else:
-        cur.execute(f'insert into infectedclients values (?, ?, ?, ?, ?)',
+        cur.execute(
+            f'insert into infectedclients values (?, ?, ?, ?, ?)',
             (log.client_mac, log.src_ip, log.detected_host, log.reason, timestamp)
         )
 
@@ -125,7 +134,7 @@ def infected_event(cur, timestamp: int, log: INF_EVENT_LOG) -> bool:
 
 @db.register('geolocation', routine_type='write')
 # first arg is timestamp. this can likely go away with new DB API.
-def geo_record(cur, _, log: GEOLOCATION_LOG) -> bool:
+def geo_record(cur: Cursor, _, log: GEOLOCATION_LOG) -> bool:
     month = ','.join(_System.date()[:2])
 
     # TODO: can this be switched to if not exists?
@@ -138,7 +147,8 @@ def geo_record(cur, _, log: GEOLOCATION_LOG) -> bool:
 
     # TODO: what does this mean? this needs to be explained better because it looks fucked up.
     # incremented count of the actions specified in the log.
-    cur.execute(f'update geolocation set {log.action}={log.action}+1 where month=? and country=? and direction=?',
+    cur.execute(
+        f'update geolocation set {log.action}={log.action}+1 where month=? and country=? and direction=?',
         (month, log.country, log.direction)
     )
 
@@ -150,7 +160,7 @@ def geo_record(cur, _, log: GEOLOCATION_LOG) -> bool:
 @db.register('clear_infected', routine_type='clear')
 # TODO: see why this wasnt being committed. i feel like it was an oversight.
 # TODO: also type this
-def clear_infected(cur, infected_client, detected_host):
+def clear_infected(cur: Cursor, infected_client, detected_host):
     cur.execute(f'delete from infectedclients where mac=? and detected_host=?', (infected_client, detected_host))
 
     return True
@@ -160,7 +170,7 @@ def clear_infected(cur, infected_client, detected_host):
 # ================================
 @db.register('blocked_domain', routine_type='query')
 # query to authorize viewing of web block page and show block info for reference
-def blocked_domain(cur, *, domain: str, src_ip: str) -> _BLOCKED_DOM:
+def blocked_domain(cur: Cursor, *, domain: str, src_ip: str) -> _BLOCKED_DOM:
     for _ in range(6):
         cur.execute(f'select * from blocked where domain=? and src_ip=?', (domain, src_ip))
         try:
@@ -170,54 +180,52 @@ def blocked_domain(cur, *, domain: str, src_ip: str) -> _BLOCKED_DOM:
 
 @db.register('last', routine_type='query')
 # most recent X matching rows
-def last(cur, count: int, src_ip: Optional[str] = None, *, table: str, action: str) -> list:
-    if (action in ['allowed', 'blocked']):
-        if (src_ip):
-            cur.execute(f'select * from {table} where src_ip=? and action=? order by last_seen desc limit {count}',
-                (src_ip, action))
-
-        else:
-            cur.execute(f'select * from {table} where action=? order by last_seen desc limit {count}', (action,))
-
-    elif (action in ['all']):
+def last(cur: Cursor, count: int, src_ip: Optional[str] = None, *, table: str, action: str) -> list:
+    if (action in ['all']):
         cur.execute(f'select * from {table} order by last_seen desc limit {count}')
+
+    elif (action in ['allowed', 'blocked']):
+        if (not src_ip):
+            cur.execute(f'select * from {table} where action=? order by last_seen desc limit {count}', (action,))
+        else:
+            cur.execute(
+                f'select * from {table} where src_ip=? and action=? order by last_seen desc limit {count}', (src_ip, action)
+            )
 
     return cur.fetchall()
 
 @db.register('top', routine_type='query')
-def top(cur, count: int, *, table: str, action: str) -> list:
-    if (action in ['allowed', 'blocked']):
-        cur.execute(f'select * from {table} where action=? order by count desc limit {count}', (action,))
-
-    elif (action in ['all']):
+def top(cur: Cursor, count: int, *, table: str, action: str) -> list:
+    if (action in ['all']):
         cur.execute(f'select * from {table} order by count desc limit {count}')
+
+    elif (action in ['allowed', 'blocked']):
+        cur.execute(f'select * from {table} where action=? order by count desc limit {count}', (action,))
 
     return cur.fetchall()
 
 @db.register('top_dashboard', routine_type='query')
-def top_dashboard(cur, count, *, action):
-    if (action in ['allowed', 'blocked']):
+def top_dashboard(cur: Cursor, count, *, action):
+    if (action in ['all']):
+        cur.execute(f'select domain, category, sum(count) from dnsproxy group by domain order by count desc limit {count}')
+
+    elif (action in ['allowed', 'blocked']):
         cur.execute(
             f'select domain, category, sum(count) from dnsproxy where action=? '
             f'group by domain order by count desc limit {count}',
             (action,)
         )
 
-    elif (action in ['all']):
-        cur.execute(
-            f'select domain, category, sum(count) from dnsproxy group by domain order by count desc limit {count}'
-        )
-
     return [(x[0], x[1]) for x in cur.fetchall()]
 
 @db.register('top_geolocation', routine_type='query')
-def top_geolocation(cur, count: int, *, action: str, direction: str) -> list[str]:
+def top_geolocation(cur: Cursor, count: int, *, action: str, direction: str) -> list[str]:
     month = ','.join(_System.date()[:2])
 
     # table has a separate column for allowed and blocked. this is why we select and sort on the action directly.
     cur.execute(
         f'select country from geolocation where month=? and direction=? and {action} > 0 '
-        f'order by {action} desc limit {count}', (month, direction, action)
+        f'order by {action} desc limit {count}', (month, direction)
     )
 
     # filtering out entries with no hits in the specified action.
@@ -225,23 +233,23 @@ def top_geolocation(cur, count: int, *, action: str, direction: str) -> list[str
 
 @db.register('unique_domain_count', routine_type='query')
 # TODO: see if this should use sum() instead of len() on the results
-def unique_domain_count(cur, *, action: str) -> int:
-    if (action in ['allowed', 'blocked']):
-        cur.execute(f'select domain, count(*) from dnsproxy where action=? group by domain', (action,))
-
-    elif (action in ['all']):
+def unique_domain_count(cur: Cursor, *, action: str) -> int:
+    if (action in ['all']):
         cur.execute(f'select domain, count(*) from dnsproxy group by domain')
+
+    elif (action in ['allowed', 'blocked']):
+        cur.execute(f'select domain, count(*) from dnsproxy where action=? group by domain', (action,))
 
     return len(cur.fetchall())
 
 @db.register('total_request_count', routine_type='query')
 # TODO: see if this should use sum() instead of iter add
-def total_request_count(cur, *, table: str, action: str) -> int:
-    if (action in ['allowed', 'blocked']):
-        cur.execute(f'select count from {table} where action=?', (action,))
-
-    elif (action in ['all']):
+def total_request_count(cur: Cursor, *, table: str, action: str) -> int:
+    if (action in ['all']):
         cur.execute(f'select count from {table}')
+
+    elif (action in ['allowed', 'blocked']):
+        cur.execute(f'select count from {table} where action=?', (action,))
 
     results = cur.fetchall()
     if (not results):
@@ -255,9 +263,10 @@ def total_request_count(cur, *, table: str, action: str) -> int:
 
 @db.register('malware_count', routine_type='query')
 # TODO: see if this should use sum() instead of iter add
-def malware_count(cur, *, table: str) -> int:
-    cur.execute(f'select * from {table} where action=? and category=? or category=?',
-        ('blocked', 'malicious', 'cryptominer'))
+def malware_count(cur: Cursor, *, table: str) -> int:
+    cur.execute(
+        f'select * from {table} where action=? and category=? or category=?', ('blocked', 'malicious', 'cryptominer')
+    )
 
     results = cur.fetchall()
     if (not results):
