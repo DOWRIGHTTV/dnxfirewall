bee9ad4537e51ef943f5fb60e7e397517b927c9b
DOWRIGHT <dowright@nationofnobodies.com>
2022-09-16 22:13:03 -0700

    theme updates

--- dnx_webui/source/main/dfe_main.py
+++ dnx_webui/source/main/dfe_main.py
@@ -848,7 +848,7 @@ def set_theme_values() -> None:
 
     if (context_global.settings['theme'] == 'dark'):
         theme.update({
-            'section_bg': 'style=background: url("static/assets/images/dnx-section-dark.png"); background-repeat: repeat',
+            'section_bg': 'style=background: url("static/assets/images/dnx_section_dark.png"); background-repeat: repeat;"',
             'main_section': 'blue-grey lighten-2',
             'off_section': 'blue-grey lighten-5',
             'card': 'blue-grey lighten-4',
@@ -857,7 +857,7 @@ def set_theme_values() -> None:
 
     elif (context_global.settings['theme'] == 'light'):
         theme.update({
-            'section_bg': 'style=background: url("static/assets/images/dnx-section-light.png"); background-repeat: repeat',
+            'section_bg': 'style="background: url(static/assets/images/dnx_section_light.png); background-repeat: repeat;"',
             'main_section': 'grey lighten-2',
             'off_section': 'grey lighten-5',
             'card': 'grey lighten-4',
