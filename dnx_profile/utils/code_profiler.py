82b83c1891411f1be47f98921c2dfdce268bcfe5
DOWRIGHT <dow@dnxfirewall.com>
2022-12-30 15:59:34 -0800

    initial rework of code profiler

--- dnx_profile/utils/code_profiler.py
+++ dnx_profile/utils/code_profiler.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import time as _time
 
+from typing import Callable
 from io import StringIO as _StringIO
 from cProfile import Profile as _Profile
 from pstats import Stats as _Stats, SortKey as _SortKey
@@ -15,26 +16,20 @@ from dnx_gentools.file_operations import append_to_file
 _fast_time_ns = _time.perf_counter_ns
 _CUMU = _SortKey.CUMULATIVE
 
-# TODO: this is broken. wtf.
-def profiler(*, filename):
-    def decorator(thing_to_be_profiled):
-        def wrapper(*args, **kwargs):
-            pr = _Profile(_fast_time_ns)
-            pr.enable()
+def dnx_profile(func: Callable, *args, filename: str, **kwargs):
+    '''profiles referenced function and saves results to specified file.
 
-            # profiled function
-            thing_to_be_profiled(*args, **kwargs)
+        path->dnx_profile/log/_tests/**filename**.cprofile
+    '''
+    with _Profile(timer=_fast_time_ns, timeunit=0.000000001) as pr:
+        func(*args, **kwargs)
 
-            pr.disable()
-            s = _StringIO()
+    s = _StringIO()
 
-            ps = _Stats(pr, stream=s).sort_stats(_CUMU)
-            ps.print_stats()
+    ps = _Stats(pr, stream=s).sort_stats(_CUMU)
+    ps.print_stats()
 
-            message = ''.join(['='*10, 'FUNCTION START', '='*10, '\n'])
+    message = ''.join(['='*10, 'RUN START', '='*10, '\n'])
 
-            append_to_file(message, filename, filepath='dnx_profile/profiler_results')
-            append_to_file(s.getvalue(), filename, filepath='dnx_profile/profiler_results')
-
-        return wrapper
-    return decorator
+    append_to_file(message, f'{filename}.profile', filepath='dnx_profile/log/_tests')
+    append_to_file(s.getvalue(), f'{filename}.profile', filepath='dnx_profile/log/_tests')
