d34a5ac74fa307313d03fdc5c12ccf2c88304d18
DOWRIGHT <dow@dnxfirewall.com>
2023-01-01 01:28:49 -0800

    cont.

--- dnx_iptools/cprotocol_tools/cprotocol_tools.pyx
+++ dnx_iptools/cprotocol_tools/cprotocol_tools.pyx
@@ -125,15 +125,21 @@ cdef class MessageQueue:
     def __dealloc__(self):
         msgctl(self.id, IPC_RMID, NULL)
 
-    def connect(self, key):
-        self.id = msgget(<key_t>key, MQ_PERMISSIONS)
+    def connect(self, pkey):
+
+        cdef key_t key = <key_t> pkey
+
+        self.id = msgget(key, MQ_PERMISSIONS)
         if (self.id == -1):
             return ERR
 
         return OK
 
-    def create_queue(self, key):
-        self.id = msgget(<key_t>key, MQ_PERMISSIONS | IPC_CREAT)
+    def create_queue(self, pkey):
+
+        cdef key_t key = <key_t> pkey
+
+        self.id = msgget(key, MQ_PERMISSIONS | IPC_CREAT)
         if (self.id == -1):
             return ERR
 
