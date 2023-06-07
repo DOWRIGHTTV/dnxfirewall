ec11f4d74b714007d5ad03573a08ff82ea0d671b
DOWRIGHT <dow@dnxfirewall.com>
2023-06-06 18:04:29 -0700

    client side form field en/disable toggling

---
+++ dnx_webui/static/assets/js/dnx_forms.js
@@ -0,0 +1,8 @@
+  function toggleFormField(btn, idArray){
+
+    let field_val = !!btn.checked ? "true" : "";
+
+    for (let id of idArray) {
+	    document.getElementById(id).disabled = field_val;
+    }
+}
