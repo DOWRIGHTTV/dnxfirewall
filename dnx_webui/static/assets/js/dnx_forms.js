024e72ada415301e79e406b5dcbf891197e821d4
DOWRIGHT <dow@dnxfirewall.com>
2023-06-09 02:11:30 -0700

    zone object updates

--- dnx_webui/static/assets/js/dnx_forms.js
+++ dnx_webui/static/assets/js/dnx_forms.js
@@ -1,6 +1,6 @@
 function toggleFormField(btn, idArray) {
 
-    let field_val = !!btn.checked ? "" : "true";
+    let field_val = !btn.checked ? "" : "true";
 
     for (let id of idArray) {
         document.getElementById(id).disabled = field_val;
