6248082e38bcd519aae628e8df71bfbb7196a8ac
DOWRIGHT <dow@dnxfirewall.com>
2023-06-09 02:29:43 -0700

    interface form improvements

--- dnx_webui/static/assets/js/dnx_forms.js
+++ dnx_webui/static/assets/js/dnx_forms.js
@@ -1,6 +1,6 @@
 function toggleFormField(condition, idArray) {
     // set disabled field based on passed in condition. if condition is true, field is disabled.
-    let field_val = !!btn.checked;
+    let field_val = !!condition;
 
     for (let id of idArray) {
         document.getElementById(id).disabled = field_val;
