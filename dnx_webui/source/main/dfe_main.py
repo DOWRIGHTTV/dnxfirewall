83ded1cb2f933446cd1eb8377e98b7ecc9f839d2
dowright <dowright@nationofnobodies.com>
2024-06-20 14:27:57 -0700

    webui dev error page update

--- dnx_webui/source/main/dfe_main.py
+++ dnx_webui/source/main/dfe_main.py
@@ -605,7 +605,15 @@ def internal_server_error(error):
     # --------------------------------------------- #
     # LABEL: DEVELOPMENT_ONLY_CODE
     if (WEBUI_DEVELOPMENT):
-        error = traceback.format_exc()
+        tb = traceback.format_exc().split('\n')
+
+        error = ''.join([
+            error.pop(0).strip(),
+            '-' * 32,
+            *[f'{s}\n' if (i % 2) else s for i, s in enumerate(error)],
+            '-' * 32,
+            error.pop(-1)
+        ])
 
         return render_template('main/dev_error.html', theme=context_global.theme, general_error=error)
     # --------------------------------------------- #
