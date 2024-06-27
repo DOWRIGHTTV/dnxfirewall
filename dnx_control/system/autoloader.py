c26d8eaca922ab437c5b69c2cb24d65f56acd87f
dowright <dowright@nationofnobodies.com>
2024-06-26 20:46:43 -0700

    cli ux improvements.

--- dnx_control/system/autoloader.py
+++ dnx_control/system/autoloader.py
@@ -66,10 +66,10 @@ def shell_run(s: str, /) -> None:
     '''
     try:
         if (args.verbose_set):
-            dnx_run(s)
+            dnx_run_v(s)
 
         else:
-            dnx_run_v(s)
+            dnx_run(s)
 
     except CalledProcessError as cpe:
         err_print(f'{cpe}')
