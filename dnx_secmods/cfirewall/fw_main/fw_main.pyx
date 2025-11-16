d5a4a035896d277d6d079f708e86d80236f06cdb
dowright <dowright@nationofnobodies.com>
2025-11-16 02:51:48 -0800

    reworked service def struct/unions.

--- dnx_secmods/cfirewall/fw_main/fw_main.pyx
+++ dnx_secmods/cfirewall/fw_main/fw_main.pyx
@@ -333,11 +333,11 @@ cdef void set_FWrule(size_t cntrl_list_idx, size_t rule_idx, dict rule):
 
         # TYPE 1 (SOLO) OBJECT ASSIGNMENT [SPECIAL CASE FOR ICMP]
         if (fw_rule.s_services.objects[i].type == SVC_SOLO):
-            fw_rule.s_services.objects[i].svc.protocol   = <uintf16_t>rule['src_service'][i][1]
+            fw_rule.s_services.objects[i].svc.protocol = <uintf16_t>rule['src_service'][i][1]
 
             if fw_rule.s_services.objects[i].svc.protocol == 1:  # ICMP Protocol
                 fw_rule.s_services.objects[i].svc.type = <uintf8_t>rule['src_service'][i][2]
-                fw_rule.s_services.objects[i].svc.code   = <uintf8_t>rule['src_service'][i][3]
+                fw_rule.s_services.objects[i].svc.code = <uintf8_t>rule['src_service'][i][3]
             else:
                 fw_rule.s_services.objects[i].svc.start_port = <uintf16_t>rule['src_service'][i][2]
                 fw_rule.s_services.objects[i].svc.end_port   = <uintf16_t>rule['src_service'][i][3]
@@ -346,7 +346,7 @@ cdef void set_FWrule(size_t cntrl_list_idx, size_t rule_idx, dict rule):
         # TYPE 2 (RANGE) OBJECT ASSIGNMENT [ICMP EXCLUDED]
         if (fw_rule.s_services.objects[i].type == SVC_RANGE):
 
-            fw_rule.s_services.objects[i].svc.protocol   = <uintf16_t>rule['src_service'][i][1]
+            fw_rule.s_services.objects[i].svc.protocol = <uintf16_t>rule['src_service'][i][1]
             if fw_rule.s_services.objects[i].svc.protocol == 1:  # ICMP Protocol
                 continue  # this should not need to be here, but will keep it while implementing.
 
@@ -359,8 +359,8 @@ cdef void set_FWrule(size_t cntrl_list_idx, size_t rule_idx, dict rule):
             for ix in range(fw_rule.s_services.objects[i].svc_list.len):
                 # [0] START INDEX ON FW RULE SIZE
                 # [1] START INDEX PYTHON DICT SIDE (to first index for size)
-                fw_rule.s_services.objects[i].svc_list.services[ix].protocol   = <uintf16_t>rule['src_service'][i][ix + 1][0]
-                if fw_rule.s_services.objects[i].svc.protocol == 1:  # ICMP Protocol
+                fw_rule.s_services.objects[i].svc_list.services[ix].protocol = <uintf16_t>rule['src_service'][i][ix + 1][0]
+                if fw_rule.s_services.objects[i].svc_list.services[ix].protocol == 1:  # ICMP Protocol
                     fw_rule.s_services.objects[i].svc_list.services[ix].type = <uintf8_t>rule['src_service'][i][ix + 1][1]
                     fw_rule.s_services.objects[i].svc_list.services[ix].code = <uintf8_t>rule['src_service'][i][ix + 1][2]
 
